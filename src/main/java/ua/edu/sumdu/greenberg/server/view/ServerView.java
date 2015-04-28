e8018ebe571b28df008cf7118d6cd42a97e96427
Greenberg Dima <gdvdima2008@yandex.ru>
2015-04-28 08:49:43 +0300

    Java Doc from ServerView

--- src/main/java/ua/edu/sumdu/greenberg/server/view/ServerView.java
+++ src/main/java/ua/edu/sumdu/greenberg/server/view/ServerView.java
@@ -1,10 +1,14 @@
 package ua.edu.sumdu.greenberg.server.view;
 
+/**
+ * This class displays messages to the console.
+ */
 public class ServerView {
-	
+	/**
+	 * This method displays a message to the console.
+	 * @param mes - message.
+	 */
 	public void consoleMessage(String mes) {
 		System.out.println(mes);
 	}
-
-	
 }
