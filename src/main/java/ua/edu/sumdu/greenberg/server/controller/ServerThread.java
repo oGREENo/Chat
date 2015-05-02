012dea732d68e84f479ca10e750e75a658dc4089
Greenberg Dima <gdvdima2008@yandex.ru>
2015-05-02 14:52:50 +0300

    The server receives the XML message.

--- src/main/java/ua/edu/sumdu/greenberg/server/controller/ServerThread.java
+++ src/main/java/ua/edu/sumdu/greenberg/server/controller/ServerThread.java
@@ -3,10 +3,18 @@ package ua.edu.sumdu.greenberg.server.controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.Socket;
 
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 import ua.edu.sumdu.greenberg.server.model.User;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 public class ServerThread implements Runnable {
 	private static final Logger log = Logger.getLogger(ServerThread.class);
@@ -27,14 +35,26 @@ public class ServerThread implements Runnable {
 	public void run() {
 
 		try {
+			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			InputSource is = new InputSource();
+
+
 			String message = null;
 			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			while ((message = bufferedReader.readLine()) != null) {
 				log.info("Message - " + message);
+				is.setCharacterStream(new StringReader(message));
+				Document doc = db.parse(is);
+				System.out.println(doc.getElementsByTagName("text").item(0).getTextContent());
+				message = null;
 			}
 			socket.close();
 		} catch (IOException e) {
 			log.error(e);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
 		}
 	}
 }
