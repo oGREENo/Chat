756a4300042bac0801d16dc645ddd4fdf6bf0610
Greenberg Dima <gdvdima2008@yandex.ru>
2015-05-12 11:02:52 +0300

    Preparing to build the project.

--- src/main/java/ua/edu/sumdu/greenberg/client/ClientChat.java
+++ src/main/java/ua/edu/sumdu/greenberg/client/ClientChat.java
@@ -1,6 +1,5 @@
 package ua.edu.sumdu.greenberg.client;
 
-import org.apache.log4j.PropertyConfigurator;
 import ua.edu.sumdu.greenberg.client.controller.ClientController;
 import ua.edu.sumdu.greenberg.client.model.ClientModel;
 import ua.edu.sumdu.greenberg.client.view.ClientViewChat;
@@ -14,7 +13,6 @@ public class ClientChat {
 	 * This is constructor a ClientChat.
 	 */
 	public ClientChat() {
-		PropertyConfigurator.configure("./src/resources/log4j.properties");
 		ClientViewLogin clientViewLogin = new ClientViewLogin();
 		ClientViewChat clientViewChat = new ClientViewChat();
 		ClientModel clientModel = new ClientModel();
--- src/main/java/ua/edu/sumdu/greenberg/server/ServerChat.java
+++ src/main/java/ua/edu/sumdu/greenberg/server/ServerChat.java
@@ -1,6 +1,5 @@
 package ua.edu.sumdu.greenberg.server;
 
-import org.apache.log4j.*;
 import ua.edu.sumdu.greenberg.server.controller.ServerController;
 import ua.edu.sumdu.greenberg.server.model.ServerModel;
 import ua.edu.sumdu.greenberg.server.view.ServerView;
@@ -13,7 +12,6 @@ public class ServerChat {
 	 * This is constructor a ServerChat.
 	 */
 	public ServerChat() {
-		PropertyConfigurator.configure("./src/resources/log4j.properties");
 		ServerView serverView = new ServerView();
 		ServerModel serverModel = new ServerModel();
 		ServerController serverController = new ServerController(serverView, serverModel);
