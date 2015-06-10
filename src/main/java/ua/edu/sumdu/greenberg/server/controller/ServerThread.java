a8b82758bd4a759759b54cecf23d28bddd62036d
GREEN <gdvdima2008@yandex.ru>
2015-06-10 12:51:53 +0300

    Refactor ServerThread.

--- src/main/java/ua/edu/sumdu/greenberg/server/controller/ServerThread.java
+++ src/main/java/ua/edu/sumdu/greenberg/server/controller/ServerThread.java
@@ -20,48 +20,50 @@ import javax.xml.parsers.ParserConfigurationException;
  * This class wait messages.
  */
 public class ServerThread implements Runnable {
-	private static final Logger log = Logger.getLogger(ServerThread.class);
-	private Socket socket;
-	private ServerModel serverModel;
+    private static final Logger log = Logger.getLogger(ServerThread.class);
+    private Socket socket;
+    private ServerModel serverModel;
 
-	/**
-	 * This is the constructor a class.
-	 * @param socket - socket.
-	 */
-	public ServerThread(Socket socket, ServerModel serverModel) {
-		this.socket = socket;
-		this.serverModel = serverModel;
-	}
+    /**
+     * This is the constructor a class.
+     *
+     * @param socket - socket.
+     */
+    public ServerThread(Socket socket, ServerModel serverModel) {
+        this.socket = socket;
+        this.serverModel = serverModel;
+    }
 
-	/**
-	 * This method starts a thread.
-	 */
-	public void run() {
-		try {
-			DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-			InputSource is = new InputSource();
-			String message;
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			while ((message = bufferedReader.readLine()) != null) {
-				is.setCharacterStream(new StringReader(message));
-				Document doc = db.parse(is);
-				serverModel.readMessage(doc, this);
-		}
-			socket.close();
-		} catch (IOException e) {
-			log.error(e);
-		} catch (ParserConfigurationException e) {
-			log.error(e);
-		} catch (SAXException e) {
-			log.error(e);
-		}
-	}
+    /**
+     * This method starts a thread.
+     */
+    public void run() {
+        try {
+            DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            InputSource is = new InputSource();
+            String message;
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while ((message = bufferedReader.readLine()) != null) {
+                is.setCharacterStream(new StringReader(message));
+                Document doc = db.parse(is);
+                serverModel.readMessage(doc, this);
+            }
+            socket.close();
+        } catch (IOException e) {
+            log.error(e);
+        } catch (ParserConfigurationException e) {
+            log.error(e);
+        } catch (SAXException e) {
+            log.error(e);
+        }
+    }
 
-	/**
-	 * This method returned a socket.
-	 * @return socket
-	 */
-	public Socket getSocket() {
-		return socket;
-	}
+    /**
+     * This method returned a socket.
+     *
+     * @return socket
+     */
+    public Socket getSocket() {
+        return socket;
+    }
 }
